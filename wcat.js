051e1616117c5f9318fb4591fd6b6dc5a810c56e
vkaash-sharma <vikasayushsharma@gmail.com>
2022-03-10 23:19:42 -0800

    wcat complete -n -b  flag is used

--- wcat.js
+++ wcat.js
@@ -10,11 +10,16 @@ let inputArr = process.argv.slice(2);
 // console.log(inputArr);
 
 let filesAr = [];
+let optionArr = [];
 
 // place the file in filesAr;
 for(let i = 0 ; i < inputArr.length ; i++ ){
-
+   let firstChar = inputArr[i].charAt(0);
+    if (firstChar == '-'){
+        optionArr.push(inputArr[i]);
+    }else{
     filesAr.push(inputArr[i]);
+    }
 }
 
 // check if all files are present or not
@@ -37,8 +42,102 @@ let content = "";
 for(let i = 0 ; i< filesAr.length ; i++){
   
     let readContent = fs.readFileSync(filesAr[i]);
-    content+=readContent+"\n";
+    content+=readContent+"\r\n";
 }
 
 console.log(content);
+
+let contentArr = content.split("\r\n");
+
+console.table(contentArr);
+
+//  let check is -s is present in the array or not;
+
+let isSPresent = optionArr.includes("-s");
+
+if(isSPresent){
+
+    for(let i = 1 ; i < contentArr.length ; i++ ){
+
+        if(contentArr[i] == "" && contentArr[i-1]==""){
+            contentArr[i] = null;
+        }else if( contentArr[i]=="" && contentArr[i-1]==null){
+            contentArr[i] = null;
+        }
+    }
+}
+
+console.table(contentArr);
+let tempArr = [];
+for(let i = 0 ; i <contentArr.length ; i++ ){
+
+    if (contentArr[i]!=null){
+        tempArr.push(contentArr[i]);
+    }
+}
+console.table(tempArr);
+contentArr = tempArr;
+
+let indexOfN = optionArr.indexOf("-n");
+let indexOfB = optionArr.indexOf("-b");
+
+// if we found -b and -n and we dont have -b and -n return -1;
+
+let finalOption ="";
+
+if (indexOfN!=-1 && indexOfB!=-1){
+
+   if(indexOfN < indexOfB){
+     finalOption = "-n";
+    }else{
+     finalOption = "-b";
+    }
+}
+else{
+  
+    if(indexOfN!=1){
+        finalOption = "-n";
+    }else if(indexOfB!=-1){
+        finalOption = "-b";
+    }
+}
+
+if(finalOption == "-n"){
+
+    modifycontentbyn();
+}else{
+    modifycontentbyb();
+}
+
+
+
+function modifycontentbyn(){
+
+    for(let i = 0 ; i< contentArr.length ; i++){
+
+        contentArr[i] = (i+ 1)+ ") "+contentArr[i];
+    }
+     
+}
+
+function modifycontentbyb(){
+
+    let count = 1;
+    for(let i = 0 ; i< contentArr.length ; i++){
+   
+        if(contentArr[i]!=""){
+        contentArr[i] = count+ ") "+contentArr[i];
+        count++;
+        }
+    }
+
+}
+
+console.log(contentArr);
+
+
+
+
+
+
 
